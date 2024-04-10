817d7fa5fc718403147f694a5df1b2416e665aa3
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-04-09 23:12:39 -0400

    Update RefdsReduxMiddleware.swift

--- Sources/RefdsRedux/RefdsReduxMiddleware.swift
+++ Sources/RefdsRedux/RefdsReduxMiddleware.swift
@@ -4,5 +4,6 @@ public typealias RefdsReduxMiddlewareCompletion = (RefdsReduxAction) -> Void
 public typealias RefdsReduxMiddleware<State> = (State, RefdsReduxAction, @escaping RefdsReduxMiddlewareCompletion) -> Void
 
 public protocol RefdsReduxMiddlewareProtocol {
-    var middleware: RefdsReduxMiddleware<RefdsReduxState> { get }
+    associatedtype State
+    var middleware: RefdsReduxMiddleware<State> { get }
 }
